2d67ede3fcf86b74c1d592f549b601022298666e
Surendhar-N-D <surendhar.n@continube.com>
2026-04-02 09:34:29 +0530

    test1

--- tools/general/command.py
+++ tools/general/command.py
@@ -422,7 +422,7 @@ async def execute_shell_command(cmd: str) -> str:
         "--dev", "/dev",
         "--"
         "bash", "-c", cmd 
-    ] + cmd
+    ]
 
     process = await asyncio.create_subprocess_exec(
         *bwrap_cmd,
